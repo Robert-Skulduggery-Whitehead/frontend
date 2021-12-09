1490c02d75d4b47fc2e681d81b398ed10adc99b3
Robert Whitehead <robert.frederik.whitehead@gmail.com>
2021-12-09 11:22:32 +0200

    Finished some more work on controls

--- src/Controls/Controls.js
+++ src/Controls/Controls.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Controls.css";
 
 let socket = require("socket.io-client")("http://127.0.0.1:3001");
 
@@ -6,69 +7,201 @@ export default class Controls extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      bestOf: "",
+      // Get teams from database (socket) and make team names drop down / text edit
+      // Even add differnt option for adding team to database
+      teams: {},
+      bestOf: 0,
+      bestOfArray: [],
       game1: {
         map: "",
         pickedBy: "",
-        score: "", //current / 16-12 / tbd
+        winnerScore: "",
+        loserScore: "",
       },
-      game2: {},
-      game3: {},
-      game4: {},
-      game5: {},
-      game6: {},
-      game7: {},
+      game2: { map: "", pickedBy: "", winnerScore: "", loserScore: "" },
+      game3: { map: "", pickedBy: "", winnerScore: "", loserScore: "" },
+      game4: { map: "", pickedBy: "", winnerScore: "", loserScore: "" },
+      game5: { map: "", pickedBy: "", winnerScore: "", loserScore: "" },
+      game6: { map: "", pickedBy: "", winnerScore: "", loserScore: "" },
+      game7: { map: "", pickedBy: "", winnerScore: "", loserScore: "" },
+      teamLeft: {
+        name: null,
+        image: null,
+      },
+      teamRight: {
+        name: null,
+        image: null,
+      },
     };
+  }
+
+  componentDidMount() {
+    socket.on("getTeams", (teams) => {
+      this.setState({
+        teamLeft: teams[0],
+        teamRight: teams[1],
+      });
+    });
+
+    socket.on("swapTeams", () => {
+      let right = this.state.teamLeft;
+      let left = this.state.teamRight;
+      this.setState({
+        teamLeft: left,
+        teamRight: right,
+      });
+    });
   }
 
   swapTeams() {
     socket.emit("swapTeams");
   }
 
-  handleChange() {}
+  setTeams() {
+    socket.emit("setTeams", [this.state.teamLeft, this.state.teamRight]);
+  }
+
+  handleChangeTeamLeft = (event) => {
+    this.setState({
+      teamLeft: {
+        name: event.target.value,
+        image: event.target.value + ".png",
+      },
+    });
+  };
+
+  handleChangeTeamRight = (event) => {
+    this.setState({
+      teamRight: {
+        name: event.target.value,
+        image: event.target.value + ".png",
+      },
+    });
+  };
+
+  handleChangeBestOf = (event) => {
+    if (
+      event.target.value === "1" ||
+      event.target.value === "3" ||
+      event.target.value === "5" ||
+      event.target.value === "7" ||
+      event.target.value === ""
+    ) {
+      this.setState({
+        bestOf: event.target.value,
+        bestOfArray: [],
+      });
+      this.setState({
+        bestOfArray: Array.apply(
+          null,
+          Array(parseInt(event.target.value) || 0)
+        ),
+      });
+    }
+
+    /*for (let i = 1; i <= event.target.value; i++) {
+      this.setState({
+        ["game" + i]: {},
+      });
+    }*/
+  };
+
+  handleChange = (name) => (event) => {
+    let temp = this.state[name.substr(0, 5)];
+    temp[name.substr(5, name.length - 5)] = event.target.value;
+    console.log(temp);
+    this.setState({
+      [name.substr(0, 5)]: temp,
+    });
+  };
 
   handleSubmit() {}
 
   render() {
     return (
       //best of x, map picks, map winners,
-      <div>
-        <form>
-          <label>Enter teams</label>
+      <div class="controlsContainer">
+        <form class="teamsForm">
+          <h4>Enter teams</h4>
+          <label>
+            Left Team name
+            <input
+              type="text"
+              name="teamLeftName"
+              value={this.state.teamLeft.name || ""}
+              onChange={this.handleChangeTeamLeft}
+            ></input>
+          </label>
+          <label>
+            Right Team name
+            <input
+              type="text"
+              name="teamLeftName"
+              value={this.state.teamRight.name || ""}
+              onChange={this.handleChangeTeamRight}
+            ></input>
+          </label>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              this.setTeams(e);
+            }}
+          >
+            Set Teams
+          </button>
         </form>
+
+        <hr class="line" />
+
         <button onClick={this.swapTeams}>Swap Teams</button>
+
+        <hr class="line" />
+
         <form onSubmit={this.handleSubmit}>
+          <h4>Game Info</h4>
           <label>
             Best of:
             <input
               type="text"
               name="BestOf"
               value={this.state.bestOf || ""}
-              onChange={this.handleChange}
+              onChange={this.handleChangeBestOf}
             ></input>
           </label>
-          <label>Game 1:</label>
-          <label>Map</label>
-          <input
-            type="text"
-            name="Game1Map"
-            value={this.state.game1.map || ""}
-            onChange={this.handleChange}
-          ></input>
-          <label>Picked By</label>
-          <input
-            type="text"
-            name="Game1Picked"
-            value={this.state.game1.pickedBy || ""}
-            onChange={this.handleChange}
-          ></input>
-          <label>Score</label>
-          <input
-            type="text"
-            name="Game1score"
-            value={this.state.game1.score || ""}
-            onChange={this.handleChange}
-          ></input>
+          <hr class="line" />
+          {this.state.bestOfArray.map((x, i) => (
+            <div>
+              <h5>{"Game " + (i + 1)}:</h5>
+              <label>Map</label>
+              <input
+                type="text"
+                name="GameMap"
+                value={this.state["game" + (i + 1)].map || ""}
+                onChange={this.handleChange("game" + (i + 1) + "map")}
+              ></input>
+              <label>Picked By</label>
+              <input
+                type="text"
+                name="GamePicked"
+                value={this.state["game" + (i + 1)].pickedBy || ""}
+                onChange={this.handleChange("game" + (i + 1) + "pickedBy")}
+              ></input>
+              <label>Winner Score</label>
+              <input
+                type="text"
+                name="Gamescore"
+                value={this.state["game" + (i + 1)].winnerScore || ""}
+                onChange={this.handleChange("game" + (i + 1) + "winnerScore")}
+              ></input>
+              <label>Loser Score</label>
+              <input
+                type="text"
+                name="Gamescore"
+                value={this.state["game" + (i + 1)].loserScore || ""}
+                onChange={this.handleChange("game" + (i + 1) + "loserScore")}
+              ></input>
+            </div>
+          ))}
         </form>
       </div>
     );
